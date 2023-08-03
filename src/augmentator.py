c3bac77bce920c0dd0cd2de11f886fcdabc67088
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-08-04 00:56:01 +0200

    Add multiple actions support

--- src/augmentator.py
+++ src/augmentator.py
@@ -11,29 +11,40 @@ class Augmentator:
         self.dir_to_strip = Path(input_dir)
         self.root_directory = Path(output_dir)
 
-    def _save_with_suffix(self, img, path: str, suffix: str):
-        """Saves image with suffix."""
+    def _get_path_with_suffix(self, path: str, suffix: str):
         directory = os.path.dirname(path)
-        directory = directory.replace(str(self.dir_to_strip), "")[1:]
+        print(directory)
+        if str(self.dir_to_strip) in path:
+            directory = directory.replace(str(self.dir_to_strip), "")[1:]
+        else:
+            print(directory)
+            directory = directory.replace(str(self.root_directory), "")[1:]
         filename = Path(path).stem
-        output = os.path.join(
+        return os.path.join(
             self.root_directory,
             Path(directory),
             Path(f"{filename}_{suffix}.png")
         )
 
+    def _save_with_suffix(self, img, path: str, suffix: str):
+        """Saves image with suffix. Returns the path under which file is saved."""
+        output = self._get_path_with_suffix(path, suffix)
         os.makedirs(os.path.dirname(output), exist_ok=True)
         img.save(output)
+        return output
 
     def greyscale(self, image_path):
         """Apply greyscale to image."""
         img = Image.open(image_path).convert('LA')
         self._save_with_suffix(img, image_path, 'greyscaled')
 
-    def rotate(self, image_path):
+    def rotate(self, image_path, apply_function=None):
         """Rotate the texture by the specified angle (degrees)."""
         degrees = [90, 180, 270]
         for rotation in degrees:
             img = Image.open(image_path)
             img = img.rotate(rotation, resample=Image.NEAREST)
-            self._save_with_suffix(img, image_path, f'rotated_{rotation}')
+            path = self._save_with_suffix(
+                img, image_path, f'rotated_{rotation}')
+            if apply_function is not None:
+                apply_function(path)
