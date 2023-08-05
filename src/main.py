0e9fb1f3b0f936e45bb329d8e53671a1666ff9fd
RooTender <lewandowski.hubert486@gmail.com>
2023-08-05 21:28:37 +0200

    Refactor main module

--- src/main.py
+++ src/main.py
@@ -1,24 +1,34 @@
 """Main module"""
 import os
 from multiprocessing import freeze_support
+from texture_tools import ArchiveHandler
 from augmentator import Utils, Techniques
+
+def unpack_textures():
+    """Unpacks the archives placed in 'data/to_unpack' folder to 'data/raw' folder."""
+    archieve_handler = ArchiveHandler()
+    input_path = os.path.join('..', 'data', 'to_unpack')
+    for archieve in os.listdir(input_path):
+        archieve_handler.extract_textures(os.path.join(input_path, archieve))
+
+def augment_datasets():
+    """Does the augmentation of a raw datasets."""
+    input_dir = os.path.join("..", "data", "raw")
+    output_dir = os.path.join("..", "data", "augmented")
+
+    techniques = Techniques(input_dir, output_dir)
+    utils = Utils(input_dir, output_dir)
+
+    utils.equalize_matching_data('original', 'x32')
+    utils.clone()
+    utils.bulk_apply(techniques.flip)
+    utils.bulk_apply(techniques.mirror)
+    utils.bulk_apply(techniques.rotate)
+    utils.bulk_apply(techniques.color_jitter,
+                        use_processed_images=True)
+    utils.bulk_apply(techniques.invert,
+                        use_processed_images=True)
 
 if __name__ == '__main__':
     # add freeze support for multiprocessing
     freeze_support()
-
-    input_dir = os.path.join("..", "data", "raw")
-    output_dir = os.path.join("..", "data", "augmented")
-
-    Aug_Techniques = Techniques(input_dir, output_dir)
-    Aug_Utils = Utils(input_dir, output_dir)
-
-    Aug_Utils.equalize_matching_data('original', 'x32')
-    Aug_Utils.clone()
-    Aug_Utils.bulk_apply(Aug_Techniques.flip)
-    Aug_Utils.bulk_apply(Aug_Techniques.mirror)
-    Aug_Utils.bulk_apply(Aug_Techniques.rotate)
-    Aug_Utils.bulk_apply(Aug_Techniques.color_jitter,
-                         use_processed_images=True)
-    Aug_Utils.bulk_apply(Aug_Techniques.invert,
-                         use_processed_images=True)
