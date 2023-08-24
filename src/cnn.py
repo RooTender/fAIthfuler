0727cd58fd157a239d9be157bb8711d5529447e4
RooTender <lewandowski.hubert486@gmail.com>
2023-08-24 11:57:22 +0200

    Improve CNN

--- src/cnn.py
+++ src/cnn.py
@@ -121,6 +121,7 @@ class CNN:
 
     def __train(self, dir_for_models: str, train_set: DataLoader, valid_set: DataLoader,
                 num_of_epochs: int, learning_rate: float):
+        path_to_save = os.path.join('..', 'models', dir_for_models)
 
         # Initialize Generator and Discriminator
         generator = FaithfulNet().cuda()
@@ -138,11 +139,17 @@ class CNN:
             return 0.8 * nn.L1Loss()(prediction, target) + 0.2 * nn.MSELoss()(prediction, target)
 
         best_loss = float('inf')
+        train_discriminator = True
+
+        total_real_loss = 0.0
+        total_fake_loss = 0.0
+        total_discriminator_loss = 0.0
 
         for epoch in range(num_of_epochs):
-            total_real_loss = 0.0
-            total_fake_loss = 0.0
-            total_discriminator_loss = 0.0
+            if train_discriminator is True:
+                total_real_loss = 0.0
+                total_fake_loss = 0.0
+                total_discriminator_loss = 0.0
 
             total_content_loss = 0.0
             total_generator_loss = 0.0
@@ -158,34 +165,11 @@ class CNN:
                 input_batch = input_batch.cuda()
                 output_batch = output_batch.cuda()
 
-                # Train Discriminator
-                discriminator_optim.zero_grad()
-
-                # Real Images
-                real_output = discriminator(output_batch)
-                real_loss = criterion_gan(
-                    real_output, torch.ones_like(real_output).cuda())
-                total_real_loss += real_loss
-
-                # Fake Images
-                fake_images = generator(input_batch)
-                fake_output = discriminator(fake_images.detach())
-                fake_loss = criterion_gan(
-                    fake_output, torch.zeros_like(fake_output).cuda())
-                total_fake_loss += fake_loss
-
-                # Combine losses and update Discriminator
-                discriminator_loss = real_loss + fake_loss
-                total_discriminator_loss += discriminator_loss
-                discriminator_loss.backward()
-                nn.utils.clip_grad.clip_grad_norm_(
-                    discriminator.parameters(), max_norm=1.0)
-                discriminator_optim.step()
-
                 # Train Generator
                 generator_optim.zero_grad()
 
                 # Content loss (MSE or L1)
+                fake_images = generator(input_batch)
                 content_loss = criterion_content(fake_images, output_batch)
                 total_content_loss += content_loss
 
@@ -202,6 +186,30 @@ class CNN:
                 nn.utils.clip_grad.clip_grad_norm_(
                     generator.parameters(), max_norm=1.0)
                 generator_optim.step()
+
+                # Train Discriminator
+                if train_discriminator:
+                    discriminator_optim.zero_grad()
+
+                    # Real Images
+                    real_output = discriminator(output_batch)
+                    real_loss = criterion_gan(
+                        real_output, torch.ones_like(real_output).cuda())
+                    total_real_loss += real_loss
+
+                    # Fake Images
+                    fake_output = discriminator(fake_images.detach())
+                    fake_loss = criterion_gan(
+                        fake_output, torch.zeros_like(fake_output).cuda())
+                    total_fake_loss += fake_loss
+
+                    # Combine losses and update Discriminator
+                    discriminator_loss = real_loss + fake_loss
+                    total_discriminator_loss += discriminator_loss
+                    discriminator_loss.backward()
+                    nn.utils.clip_grad.clip_grad_norm_(
+                        discriminator.parameters(), max_norm=1.0)
+                    discriminator_optim.step()
 
                 total_batches += 1
 
@@ -225,62 +233,89 @@ class CNN:
                                  "Loss": avg_loss}})
 
             with torch.no_grad():
-                total_fake_loss = 0.0
-                total_real_loss = 0.0
-                total_validation_loss = 0.0
+                total_content_loss = 0.0
+                total_gan_loss = 0.0
+                total_generator_loss = 0.0
+                total_discriminator_loss = 0.0
 
                 total_validation_batches = 0
 
-                for (input_batch, output_batch) in tqdm(valid_set, unit="batch", desc=f'validating epoch {epoch}'):
+                for (input_batch, output_batch) in tqdm(
+                    valid_set,
+                    unit="batch",
+                    desc=f'validating epoch {epoch}'):
                     input_batch = input_batch.cuda()
                     output_batch = output_batch.cuda()
 
                     # Forward pass
                     fake_images = generator(input_batch)
+
+                    # Content loss (MSE or L1)
+                    content_loss = criterion_content(fake_images, output_batch)
+                    total_content_loss += content_loss
+
+                    # GAN loss
+                    generator_fake_output = discriminator(fake_images)
+                    gan_loss = criterion_gan(
+                        generator_fake_output, torch.ones_like(generator_fake_output).cuda())
+                    total_gan_loss += gan_loss
+
                     real_output = discriminator(output_batch)
                     fake_output = discriminator(fake_images.detach())
 
-                    # Compute losses (similar to your training loop but without updates)
+                    # Combine losses for Generator
+                    generator_loss = content_loss + gan_loss
+                    total_generator_loss += generator_loss
+
+                    # Discriminator loss
                     real_loss = criterion_gan(
                         real_output, torch.ones_like(real_output).cuda())
                     fake_loss = criterion_gan(
                         fake_output, torch.zeros_like(fake_output).cuda())
-                    validation_loss = real_loss + fake_loss
+                    total_discriminator_loss = real_loss + fake_loss
 
-                    total_validation_loss += validation_loss
                     total_validation_batches += 1
 
-                avg_validation_loss = total_validation_loss / total_validation_batches
-                avg_real_loss = total_real_loss / total_validation_batches
-                avg_fake_loss = total_fake_loss / total_validation_batches
+                avg_content_loss = total_content_loss / total_validation_batches
+                avg_gan_loss = total_gan_loss / total_validation_batches
+                avg_generator_loss = total_generator_loss / total_validation_batches
+                avg_discriminator_loss = total_discriminator_loss / total_validation_batches
 
-                wandb.log({"validation": {"Loss": avg_validation_loss,
-                                          "Real images loss": avg_real_loss,
-                                          "Fake images loss": avg_fake_loss}})
+                wandb.log({"validation": {"Content loss": avg_content_loss,
+                                          "GAN loss": avg_gan_loss,
+                                          "Generator loss": avg_generator_loss,
+                                          "Discriminator loss": avg_discriminator_loss}})
+                
+                train_discriminator = bool(avg_discriminator_loss > 1)
 
-            if avg_loss < best_loss:
-                best_loss = avg_loss
+            if epoch > 50 and avg_generator_loss < best_loss:
+                best_loss = avg_generator_loss
 
                 os.makedirs(os.path.join('..', 'models',
                             dir_for_models), exist_ok=True)
-                path_to_save = os.path.join('..', 'models', dir_for_models)
                 torch.save(generator.state_dict(), os.path.join(
-                    path_to_save, f'G_e{epoch}_{avg_generator_loss:.4f}.pth'))
+                    path_to_save, f'e{epoch}_D_{avg_generator_loss:.4f}.pth'))
                 torch.save(discriminator.state_dict(), os.path.join(
-                    path_to_save, f'D_e{epoch}_{avg_discriminator_loss:.4f}.pth'))
+                    path_to_save, f'e{epoch}_G_{avg_discriminator_loss:.4f}.pth'))
+
+        os.makedirs(os.path.join('..', 'models',
+                                dir_for_models), exist_ok=True)    
+        torch.save(generator.state_dict(), os.path.join(
+            path_to_save, 'e_last_D_final.pth'))
+        torch.save(discriminator.state_dict(), os.path.join(
+            path_to_save, 'e_last_G_final.pth'))
 
     def run(self, input_path: str, output_path: str):
         """Just a test..."""
 
         # SGD performs the best on 0.1
         # Adam on 0.001
-        learning_rate = 0.005
+        learning_rate = 0.001
 
         # SGD on 16
         # Adam on ???
         batch_size = 64
-        epochs = 100
-        optimizer = 'sgd'
+        epochs = 1000
 
         dimensions = os.path.basename(os.path.normpath(input_path))
 
@@ -295,8 +330,7 @@ class CNN:
                 "architecture": "CNN",
                 "learning_rate": learning_rate,
                 "batch_size": batch_size,
-                "hidden_layer_sizes": [64],
-                "optimizer": optimizer
+                "optimizer": "Adam"
             }
         )
 
@@ -317,36 +351,3 @@ class CNN:
                      train_set, validation_set, epochs, learning_rate)
 
         wandb.finish()
-
-    def generate_image(self, input_image_path: str, model_path="faithfulnet.pth"):
-        """Generates an image using a GAN generator model."""
-
-        # Load the FaithfulNet model and its trained weights
-        model = FaithfulNet()
-        model.load_state_dict(torch.load(model_path))
-        model.cuda()
-        model.eval()
-
-        # Load and preprocess the input image
-        input_image = Image.open(input_image_path)
-        transform = transforms.Compose([
-            transforms.ToTensor()
-        ])
-        input_tensor = transform(input_image).unsqueeze(  # type: ignore
-            0).cuda()
-
-        # Generate the image
-        with torch.no_grad():
-            generated_image = model(input_tensor)
-
-        # Post-process the generated image
-        # Rescale from [-1, 1] to [0, 1]
-        generated_image = generated_image.squeeze().cpu().detach()
-        generated_image = (generated_image + 1) / 2.0
-        generated_image = transforms.ToPILImage()(generated_image)
-        # Convert the tensor to a PIL image (assuming it's an RGB image)
-
-        os.makedirs(os.path.join('..', 'out'), exist_ok=True)
-        generated_image.save(os.path.join(
-            '..', 'out', os.path.basename(input_image_path)))
-        generated_image.close()
